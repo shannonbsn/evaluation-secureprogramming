ad55f97a638fd9282f71039358a572fd7303ab58
Shannon Besson <shannon0413@outlook.com>
2025-06-17 16:14:12 +0200

    (feat(Hashing):Configuration du service hashing)

--- src/services/hashing.ts
+++ src/services/hashing.ts
@@ -1,7 +1,11 @@
 // Ici dénissez les fonctions qui vont vous permettre de vérifier et générer des mots de passes sécurisés
+import * as argon2 from 'argon2';
+import { randomBytes } from 'crypto';
 
 export const hashPassword = async (password: string): Promise<{ password: string, salt: string }> => {
-
+  const salt = randomBytes(10).toString('hex');
+  const hashedPassword = await argon2.hash(password + salt);
+  return { password: hashedPassword, salt };
 };
 
 export const verifyPassword = async (
@@ -9,4 +13,5 @@ export const verifyPassword = async (
   salt: string,
   hashedPassword: string
 ): Promise<boolean> => {
+  return await argon2.verify(hashedPassword, password + salt);
 };
